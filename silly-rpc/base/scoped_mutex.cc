697d3591d78e39596fbae55f0693c5f259986f28
lilde90 <leepan1990@gmail.com>
2018-01-04 23:17:10 +0800

    add scoped mutex

---
+++ silly-rpc/base/scoped_mutex.cc
@@ -0,0 +1,8 @@
+// Copyright 2018 lilde90. All Rights Reserved.
+// Author: Pan Li (panli.me@gmail.com)
+//
+#include "silly-rpc/base/scoped_mutex.h"
+namespace sillyrpc {
+namespace base {
+} // namespace base
+} // namespace sillyrpc
---
+++ silly-rpc/base/scoped_mutex.h
@@ -0,0 +1,46 @@
+// Copyright 2018 lilde90. All Rights Reserved.
+// Author: Pan Li (panli.me@gmail.com)
+//
+#ifndef _SILLYRPC_BASE_SCOPED_MUTEX_H_
+#define _SILLYRPC_BASE_SCOPED_MUTEX_H_
+#include <pthread.h>
+namespace sillyrpc {
+namespace base {
+
+class ScopedMutex {
+public:
+  ScopedMutex() {
+    pthread_mutex_init(&_mutex, NULL);
+  }
+  ~ScopedMutex() {
+    pthread_mutex_destroy(&_mutex);
+  }
+
+  void lock() {
+    pthread_mutex_lock(&_mutex);
+  }
+
+  void unlock() {
+    pthread_mutex_unlock(&_mutex);
+  }
+
+private:
+  pthread_mutex_t _mutex;
+};
+
+class ScopedMutexGuard {
+public:
+  ScopedMutexGuard(ScopedMutex& mutex) :
+    _mutex(mutex) {
+    _mutex.lock();
+  }
+  ~ScopedMutexGuard() {
+    _mutex.unlock();
+  }
+private:
+  ScopedMutex _mutex;
+};
+
+} // namespace base
+} // namespace sillyrpc
+#endif // _SILLYRPC_BASE_SCOPED_MUTEX_H_
