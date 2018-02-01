80873f3e764410ffacf24a20265a25a3b6767c84
lilde90 <leepan1990@gmail.com>
2018-02-01 21:54:22 +0800

    add tcp server member

--- silly-rpc/core/tcp_server.h
+++ silly-rpc/core/tcp_server.h
@@ -4,10 +4,16 @@
 #ifndef _SILLYRPC_CORE_TCP_SERVER_H_
 #define _SILLYRPC_CORE_TCP_SERVER_H_
 
+#include <string>
+#include <silly-rpc/core/event_loop.h>
+
 namespace sillyrpc {
 namespace core {
 
 class TcpServer {
+private:
+  EventLoop* _loop;
+  const std::string _name;
 };
 
 } // namespace core
